308036f6930c252f4dc9d1ff43cc25e513f05cef
developdaly <developdaly@9eefaf49-d13b-4547-b2ae-f4dffa355a4c>
2012-10-19 04:03:17 +0000

    Filter markup and class cleanup
    git-svn-id: https://developdaly.svn.beanstalkapp.com/create-denton/trunk@41 9eefaf49-d13b-4547-b2ae-f4dffa355a4c

--- content/themes/creatives/sidebar-filter.php
+++ content/themes/creatives/sidebar-filter.php
@@ -1,14 +1,14 @@
 <li class="item filters">
 	<ul id="filterOptions" class="nav nav-list">
-		<li class="bg-gradient-1 illustrator"><a href="#">Illustrator</a></li>
-        <li class="bg-gradient-2"><a href="#">Photographer</a></li>
-        <li class="bg-gradient-3"><a href="#">Programmer</a></li>
-        <li class="bg-gradient-4"><a href="#">Film/Video</a></li>
-        <li class="bg-gradient-5"><a href="#">Writer</a></li>
-        <li class="bg-gradient-6 web-designer"><a href="#">Web Designer</a></li>
-        <li class="bg-gradient-7 graphic-designer"><a href="#">Graphic Design</a></li>
-        <li class="bg-gradient-8"><a href="#">Fine Art</a></li>
-        <li class="bg-gradient-9"><a href="#">Other</a></li>
+		<li class="illustrator"><span class="legend"></span><a href="#">Illustrator</a></li>
+        <li class="photographer"><span class="legend"></span><a href="#">Photographer</a></li>
+        <li class="programmer"><span class="legend"></span><a href="#">Programmer</a></li>
+        <li class="film-video"><span class="legend"></span><a href="#">Film/Video</a></li>
+        <li class="writer"><span class="legend"></span><a href="#">Writer</a></li>
+        <li class="web-designer"><span class="legend"></span><a href="#">Web Designer</a></li>
+        <li class="graphic-designer"><span class="legend"></span><a href="#">Graphic Design</a></li>
+        <li class="fine-art"><span class="legend"></span><a href="#">Fine Art</a></li>
+        <li class="other"><span class="legend"></span><a href="#">Other</a></li>
         <li><a href="#">Reset</a></li>
 	</ul>
 </li>
