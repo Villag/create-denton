5396f6858821a50d9c803300f38f6ba1d9b86aed
developdaly <developdaly@9eefaf49-d13b-4547-b2ae-f4dffa355a4c>
2012-10-19 13:29:47 +0000

    Detach modal from its container and append it to the body so the blur is behind the modal. Using native sign up and login forms.
    git-svn-id: https://developdaly.svn.beanstalkapp.com/create-denton/trunk@44 9eefaf49-d13b-4547-b2ae-f4dffa355a4c

--- content/themes/creatives/js/app.js
+++ content/themes/creatives/js/app.js
@@ -7,6 +7,8 @@ $(document).ready(function() {
 
 	$('.modal').on('show', function () {
 		$('.site').blurjs();
+		$(this).detach();
+		$(this).append('body');
 	});
 	$('.modal').on('hide', function () {
 		$.blurjs('reset');
