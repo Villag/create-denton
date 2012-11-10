3c8d0ad304769e6e99fdf1d0931f7dd04b5ef9bc
developdaly <developdaly@9eefaf49-d13b-4547-b2ae-f4dffa355a4c>
2012-11-10 05:20:41 +0000

    Added real page template
    git-svn-id: https://developdaly.svn.beanstalkapp.com/create-denton/trunk@106 9eefaf49-d13b-4547-b2ae-f4dffa355a4c

--- content/themes/createdenton/page.php
+++ content/themes/createdenton/page.php
@@ -1,37 +1,67 @@
 <?php
 /**
- * The Template for displaying all single posts.
+ * Page Template
  *
- * @package WordPress
- * @subpackage Twenty_Twelve
- * @since Twenty Twelve 1.0
+ * This is the default page template.  It is used when a more specific template can't be found to display
+ * posts.  It is unlikely that this template will ever be used, but there may be rare cases.
+ *
+ * @package News
+ * @subpackage Template
  */
 
 get_header(); ?>
 
-	<div id="primary" class="site-content">
-		<div id="content" role="main">
+	<?php do_atomic( 'before_content' ); // Before content hook ?>
 
-			<?php while ( have_posts() ) : the_post(); ?>
+	<div id="content">
 
-				<?php get_template_part( 'content', get_post_format() ); ?>
+		<?php do_atomic( 'open_content' ); // Open content hook ?>
 
-				<nav class="nav-single">
-					<h3 class="assistive-text"><?php _e( 'Post navigation', 'twentytwelve' ); ?></h3>
-					<span class="nav-previous"><?php previous_post_link( '%link', '<span class="meta-nav">' . _x( '&larr;', 'Previous post link', 'twentytwelve' ) . '</span> %title' ); ?></span>
-					<span class="nav-next"><?php next_post_link( '%link', '%title <span class="meta-nav">' . _x( '&rarr;', 'Next post link', 'twentytwelve' ) . '</span>' ); ?></span>
-				</nav><!-- .nav-single -->
+		<div class="hfeed">
 
-				<?php
-					// If comments are open or we have at least one comment, load up the comment template
-					if ( comments_open() || '0' != get_comments_number() )
-						comments_template( '', true );
-				?>
+			<?php get_template_part( 'loop-meta' ); ?>
 
-			<?php endwhile; // end of the loop. ?>
+			<?php if ( have_posts() ) : ?>
 
-		</div><!-- #content -->
-	</div><!-- #primary -->
+				<?php while ( have_posts() ) : the_post(); ?>
 
-<?php get_sidebar(); ?>
+					<?php do_atomic( 'before_entry' ); // Before entry hook ?>
+
+					<div id="post-<?php the_ID(); ?>" class="<?php hybrid_entry_class(); ?>">
+
+						<?php do_atomic( 'open_entry' ); // Open entry hook ?>
+
+						<?php echo apply_atomic_shortcode( 'entry_utility', '<div class="entry-utility">' . __( '[entry-popup-shortlink]', 'news' ) . '</div>' ); ?>
+
+						<?php echo apply_atomic_shortcode( 'entry_title', '[entry-title]' ); ?>
+
+						<?php echo apply_atomic_shortcode( 'byline', '<div class="byline">' . __( 'By [entry-author] on [entry-published] [entry-terms taxonomy="category" before=" in "] [entry-edit-link before=" | "]', 'news' ) . '</div>' ); ?>
+
+						<div class="entry-content">
+							<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'news' ) ); ?>
+							<?php wp_link_pages( array( 'before' => '<p class="page-links">' . __( 'Pages:', 'news' ), 'after' => '</p>' ) ); ?>
+						</div><!-- .entry-content -->
+
+						<?php echo apply_atomic_shortcode( 'entry_meta', '<div class="entry-meta">' . __( '<span class="share">Share this on:</span> [entry-mixx-link] [entry-delicious-link] [entry-digg-link] [entry-facebook-link] [entry-twitter-link]', 'news' ) . '</div>' ); ?>
+
+						<?php do_atomic( 'close_entry' ); // Close entry hook ?>
+
+					</div><!-- .hentry -->
+
+					<?php do_atomic( 'after_entry' ); // After entry hook ?>
+
+				<?php endwhile; ?>
+
+			<?php endif; ?>
+
+		</div><!-- .hfeed -->
+
+		<?php do_atomic( 'close_content' ); // Close content hook ?>
+
+		<?php get_template_part( 'loop-nav' ); ?>
+
+	</div><!-- #content -->
+
+	<?php do_atomic( 'after_content' ); // After content hook ?>
+
 <?php get_footer(); ?>
