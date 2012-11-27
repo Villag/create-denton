956bd45088c20701dc7c419cc00b03c559194289
developdaly <developdaly@9eefaf49-d13b-4547-b2ae-f4dffa355a4c>
2012-11-27 04:28:06 +0000

    Removed stray content directory
    git-svn-id: https://developdaly.svn.beanstalkapp.com/create-denton/trunk@178 9eefaf49-d13b-4547-b2ae-f4dffa355a4c

--- content/content/themes/createdenton/first-timer.php
+++
@@ -1,8 +0,0 @@
-<script type="text/javascript">
-	jQuery(document).ready(function($) {
-
-		// Display a modal if the ID matches the hash in the URL
-		$('#about').reveal('open');
-
-	});
-</script>
